967f2578da2726b8dca300c93d5362e81c610d4e
SIlakNgoding <72643154+SilakNgoding@users.noreply.github.com>
2020-10-23 21:34:02 +0800

    Create Main_perulangan.java

---
+++ Main_perulangan.java
@@ -0,0 +1,14 @@
+package perulangan;
+
+public class Main_Perulangan {
+
+    public static void main(String[] args) {
+        Perulangan x = new Perulangan();
+        x.perulangan1();
+        x.perulangan2();
+        x.perulangan3();
+        x.perulangan4();
+
+    }
+
+}
