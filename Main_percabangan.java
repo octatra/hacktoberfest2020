48f477afe6a62e74895201e61cf44b675a1caca7
SIlakNgoding <72643154+SilakNgoding@users.noreply.github.com>
2020-10-23 21:35:58 +0800

    Create Main_percabangan.java

---
+++ Main_percabangan.java
@@ -0,0 +1,14 @@
+package percabangan;
+
+public class Main_percabagan {
+
+    public static void main(String[] args) {
+        Percabangan x = new Percabangan();
+        x.percabangan1();
+        x.percabangan2();
+        x.percabangan3();
+        x.percabangan4();
+
+    }
+
+}
